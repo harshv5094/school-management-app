a14e757a150e9f778fcae49af4226588b8f19a00
harshv5904 <ardostree2001@outlook.com>
2023-10-17 17:05:57 +0530

    feat: Fetching Student Information By Calling API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react"
 import Header from "./components/Header"
+import { fetchStudentsInformation } from "./features/studentReducer"
+import { useDispatch, useSelector } from "react-redux"
 
 function App() {
+  const dispatch = useDispatch()
+  const { students, status, error } = useSelector(state => state.students)
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchStudentsInformation())
+    }
+  }, [status, dispatch])
   return (
     <div>
       <Header />
-      <h1>App</h1>
     </div>
   )
 }
