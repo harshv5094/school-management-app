148e7916c90a71641e69a0217cfbe6a911379a3a
harshv5904 <ardostree2001@outlook.com>
2023-10-17 16:59:59 +0530

    feat: Creating header section skeleton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 function Header() {
   return (
-    <div>
-      <h1 className="text-center text-3xl">Header</h1>
-    </div>
+    <nav className="flex flex-row justify-between z-30 backdrop-blur-sm fixed w-full">
+      <h1 className="text-4xl md:text-3xl px-10 py-2">School Management</h1>
+      <div className="flex flex-row px-8 md:px-8 py-3 text-[20px] leading-9 overflow-auto">
+      </div>
+    </nav>
   )
 }
 
