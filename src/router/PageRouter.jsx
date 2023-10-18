b2b10f5aae6f2f8e5ad932fffcc61303df615470
harshv5904 <ardostree2001@outlook.com>
2023-10-18 17:42:11 +0530

    feat: Creating page routes for new pages

--- src/router/PageRouter.jsx
+++ src/router/PageRouter.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import StudentView from "../pages/StudentView"
 import PageNotFound from "../pages/404"
+import Home from "../pages/Home"
+import TeacherView from "../pages/TeacherView"
 
 function PageRouter() {
   return (
     <Routes>
-      <Route path="/" element={<StudentView />} />
+      <Route path="/" element={<Home />} />
       <Route path="/*" element={<PageNotFound />} />
+      <Route path="/students" element={<StudentView />} />
+      <Route path="/teachers" element={<TeacherView />} />
     </Routes>
   )
 }
