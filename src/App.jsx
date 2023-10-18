fc7352add145b532f1d80287ebca38bc4e587c4c
harshv5904 <ardostree2001@outlook.com>
2023-10-18 17:49:18 +0530

    feat: Importing NavBar and pageRouter in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useEffect } from "react"
-import Header from "./components/Header"
-import { fetchStudentsInformation } from "./features/studentReducer"
-import { useDispatch, useSelector } from "react-redux"
+import Navbar from "./components/navbar"
+import PageRouter from "./router/PageRouter"
 
 function App() {
-  const dispatch = useDispatch()
-  const { students, status, error } = useSelector(state => state.students)
-  useEffect(() => {
-    if (status === "idle") {
-      dispatch(fetchStudentsInformation())
-    }
-  }, [status, dispatch])
   return (
-    <div>
-      <Header />
-    </div>
+    <main>
+      <Navbar />
+      <PageRouter />
+    </main>
   )
 }
 
