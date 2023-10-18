57c2f6025b9bf5a8d3bdf59f4de95efdcb65517a
harshv5904 <ardostree2001@outlook.com>
2023-10-18 17:40:24 +0530

    feat: Connecting Chakra UI with react app

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { Provider } from 'react-redux'
 import store from './app/store.js'
 import App from './App.jsx'
 import './index.css'
+import ChakraUI from './components/chakra';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <ChakraUI>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </ChakraUI>
   </React.StrictMode>,
 )
