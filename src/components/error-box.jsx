a1e9f488db51ad287e03a6fc09f5add5495242a7
harshv5904 <ardostree2001@outlook.com>
2023-10-20 14:24:36 +0530

    chore: Refactoring Error Box Size

--- src/components/error-box.jsx
+++ src/components/error-box.jsx
@@ -1,26 +1,31 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle } from "@chakra-ui/react";
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Container } from "@chakra-ui/react";
 
 
 function Error({ message }) {
   return (
-    <Alert
-      marginTop={`4rem`}
-      status={`error`}
-      variant={`subtle`}
-      flexDir={`column`}
-      alignItems={`center`}
-      justifyContent={`center`}
-      textAlign={`center`}
-      height={`200px`}
+    <Container
+      mt={`3rem`}
+      size={`container.lg`}
+      centerContent
     >
-      <AlertIcon boxSize={`40px`} mr={0} />
-      <AlertTitle mt={4} mb={1} fontSize={`lg`}>
-        Error fetching data from from the server!
-      </AlertTitle>
-      <AlertDescription>
-        {message}
-      </AlertDescription>
-    </Alert>
+      <Alert
+        status={`error`}
+        variant={`subtle`}
+        flexDir={`column`}
+        alignItems={`center`}
+        justifyContent={`center`}
+        textAlign={`center`}
+        height={`200px`}
+      >
+        <AlertIcon boxSize={`40px`} mr={0} />
+        <AlertTitle mt={4} mb={1} fontSize={`lg`}>
+          Error From The Server!
+        </AlertTitle>
+        <AlertDescription>
+          {message}
+        </AlertDescription>
+      </Alert>
+    </Container>
   )
 }
 
