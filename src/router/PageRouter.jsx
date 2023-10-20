a0033bf51341e949036f87386425d794464493c7
harshv5904 <ardostree2001@outlook.com>
2023-10-20 14:28:04 +0530

    feat: Adding New Pages to routes

--- src/router/PageRouter.jsx
+++ src/router/PageRouter.jsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router-dom"
-import StudentView from "../pages/StudentView"
 import PageNotFound from "../pages/404"
 import Home from "../pages/Home"
+import StudentsList from "../pages/StudentsList"
+import TeachersList from "../pages/TeachersList"
+import StudentView from "../pages/StudentView"
 import TeacherView from "../pages/TeacherView"
 
 function PageRouter() {
@@ -9,8 +11,10 @@ function PageRouter() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/*" element={<PageNotFound />} />
-      <Route path="/students" element={<StudentView />} />
-      <Route path="/teachers" element={<TeacherView />} />
+      <Route path="/students" element={<StudentsList />} />
+      <Route path="/students/:studentID" element={<StudentView />} />
+      <Route path="/teachers" element={<TeachersList />} />
+      <Route path="/teachers/:teacherID" element={<TeacherView />} />
     </Routes>
   )
 }
